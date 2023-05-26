e8031177c9ef522dc5bb294820450d9dea843243
AbimannanAnbalagan <127296973+AbimannanAnbalagan@users.noreply.github.com>
2023-05-26 15:33:25 +0530

    JWT migrated and controllers are created
    Auth Folder is created & migrated for JWT and controllers also created

---
+++ Hotel Booking System/Controllers/HotelsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Hotel_Booking_System.Models;
+
+namespace Hotel_Booking_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        private readonly FinalDbContext _context;
+
+        public HotelsController(FinalDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Hotels
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Hotels>>> GetHotels()
+        {
+          if (_context.Hotels == null)
+          {
+              return NotFound();
+          }
+            return await _context.Hotels.ToListAsync();
+        }
+
+        // GET: api/Hotels/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Hotels>> GetHotels(int id)
+        {
+          if (_context.Hotels == null)
+          {
+              return NotFound();
+          }
+            var hotels = await _context.Hotels.FindAsync(id);
+
+            if (hotels == null)
+            {
+                return NotFound();
+            }
+
+            return hotels;
+        }
+
+        // PUT: api/Hotels/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHotels(int id, Hotels hotels)
+        {
+            if (id != hotels.HotelId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(hotels).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HotelsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Hotels
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Hotels>> PostHotels(Hotels hotels)
+        {
+            /*
+          if (_context.Hotels == null)
+          {
+              return Problem("Entity set 'FinalDbContext.Hotels'  is null.");
+          }
+            _context.Hotels.Add(hotels);
+            await _context.SaveChangesAsync();
+            */
+            return await _context.PostHotels(hotels);
+        }
+
+        // DELETE: api/Hotels/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHotels(int id)
+        {
+            if (_context.Hotels == null)
+            {
+                return NotFound();
+            }
+            var hotels = await _context.Hotels.FindAsync(id);
+            if (hotels == null)
+            {
+                return NotFound();
+            }
+
+            _context.Hotels.Remove(hotels);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool HotelsExists(int id)
+        {
+            return (_context.Hotels?.Any(e => e.HotelId == id)).GetValueOrDefault();
+        }
+    }
+}
